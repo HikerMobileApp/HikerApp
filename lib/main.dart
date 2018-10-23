4425de5612c6cac66b57823692097a284e087052
Jacob M Rohweder <git@gitlab.cs.plu.edu:390Group2/GoMoKu.git>
2018-10-23 14:59:34 -0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 const Color dark_green = Color(0xff027206);
 const Color jade_blue = Color(0xff339192);
 
-//List<Widget> cards = new List.generate(20, (i)=>new HikeCard());
-<<<<<<< HEAD
-//String hikeName;
-//String typeOfHike;
-=======
+//List<Widget> cards = new List.generate(20, (i)=>new HikeCard()); test
 String hikeName;
 String typeOfHike;
->>>>>>> 4e363aaf7366103a09cb8d3071dd6b2bc16403b9
 
 void main() => runApp( new MaterialApp(
   theme: new ThemeData(
