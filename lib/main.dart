55573bf4fc88fec215e98f67a2e265dccb548e37
Robin <naggirk@plu.edu>
2018-10-29 20:34:27 -0700

    change

--- lib/main.dart
+++ lib/main.dart
@@ -72,30 +72,3 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     );
   }
 }
-
-
-
-class HikeList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('Hikes').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting: return new Text('Loading...');
-          default:
-            return new ListView(
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
-                return new ListTile(
-                  title: new Text(document['Title']),
-                  subtitle: new Text(document['Type']),
-                );
-              }).toList(),
-            );
-        }
-      },
-    );
-  }
-}
