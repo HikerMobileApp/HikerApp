ebb8af523fc6e1e5ddd49c6714e4c98991832f5d
Robin <naggirk@plu.edu>
2018-10-31 15:36:58 -0700

    Textfields in AddHIkePage that will send the field data to the database.

--- lib/AddHikePage.dart
+++ lib/AddHikePage.dart
@@ -6,37 +6,64 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class AddHikePage extends MaterialPageRoute<Null>{
   
   AddHikePage(): super( builder: (BuildContext context){
-    final myController = TextEditingController();
+    final hikeName = TextEditingController();  // hike name
+    final hikeType = TextEditingController();  // hike type 
 
     return new Scaffold(
-  appBar: new AppBar(
-    backgroundColor: dark_green,
-    title: new Text("Add a hike"),
-    actions: <Widget>[
-      new IconButton(icon: const Icon(Icons.check), onPressed: () {
+      appBar: new AppBar(
+        backgroundColor: dark_green,
+        title: new Text("Add a hike"),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.check), onPressed: () {
         
-        if(myController.text != "")
-        {
-          Firestore.instance.collection("Hiking").document().setData({'Title': myController.text, 'Type': 'Being HELLA Coool!!!!!!!'});
-          //print(myController.text);
-          Navigator.pop(context);
-        }
-      })
+            if(hikeName.text != "" && hikeType.text != "")
+            {   
+              Firestore.instance.collection("Hiking").document().setData({'Title': hikeName.text, 'Type': hikeType.text});
+              //print(myController.text);
+              Navigator.pop(context);
+            }
+          })
+        ],
+      ),
+    body: new Column(
+    children: <Widget>[
+      new ListTile(
+        leading: const Icon(Icons.directions_walk),
+        title: new TextField(
+          decoration: new InputDecoration(
+            labelText: 'Hike Name',
+            hintText: "Name your hike",
+          ),
+          controller: hikeName,
+        ),
+      ),
+      new ListTile(
+        leading: const Icon(Icons.directions_walk),
+        title: new TextField(
+          decoration: new InputDecoration(
+            labelText: 'Hike Type',
+            hintText: "Describe the hike",
+          ),
+          controller: hikeType,
+        ),
+      ),
     ],
   ),
-  body: new Container(
+    );
+  });
+}
+
+
+
+
+/*new Container(
     padding: new EdgeInsets.all(10.0),
       child: TextField(
         decoration: InputDecoration( 
           icon: Icon(Icons.directions_walk),
           labelText: 'Hike Name',
           hintText: 'Please enter a search term',
-          
         ),
-          controller: myController,
+          controller: hikeName,
       ),
-  ),
-);
-  } 
-  );
-}
+  ),*/
