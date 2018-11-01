0e9c396603f89c0745cec37fbd5fa7dd96dec816
Robin <naggirk@plu.edu>
2018-11-01 00:34:18 -0700

    Testing

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,14 +1,19 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+@import UIKit;
+@import Firebase;
 
 @implementation AppDelegate
+
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     UIApplication.sharedApplication.statusBarHidden = false;
+    [FIRApp configure];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
 
 @end
