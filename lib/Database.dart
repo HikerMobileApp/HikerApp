9dd89da09f901d8f8f3fa4920c171a743c3db509
Robin <naggirk@plu.edu>
2018-12-13 22:52:09 -0800

    If logined in  with facebook database will display the users name now

--- lib/Database.dart
+++ lib/Database.dart
@@ -6,52 +6,27 @@ class Database{
 
 FirebaseUser mCurrentUser;
 FirebaseAuth _auth = FirebaseAuth.instance;
-  //I need to rethink this to a bool
-  void pushAddHike(String user, String hikeName, String hikeType)
-  {
-    _getCurrentUser();
-    print('Hello ' + mCurrentUser.displayName.toString());
-    Firestore.instance
-    .collection(mCurrentUser.displayName.toString())
-    .document("Hikes To Do")
-    .collection("Hike List")
-    .document(hikeName)
-    .setData({'Title': hikeName,'Type': hikeType});
-  }
 
-  
-  _getCurrentUser () async {
-  mCurrentUser = await _auth.currentUser();
-  print('Hello ' + mCurrentUser.displayName.toString());
-  }
-
-  void pullHikes()
-  {
-    String user = FirebaseAuth.instance.currentUser().toString();
-    CollectionReference collectionRef = Firestore.instance
-    .collection(user)
-    .document("Hikes To Do")
-    .collection("Hike List");
-
-    collectionRef.getDocuments();
-
-
-  Future <List<Map<dynamic, dynamic>>> getCollection() async{
-  List<DocumentSnapshot> templist;
-  List<Map<dynamic, dynamic>> list = new List();
-  CollectionReference collectionRef = Firestore.instance.collection(user)
-                                                        .document("Hikes To Do")
-                                                        .collection("Hike List");
-  QuerySnapshot collectionSnapshot = await collectionRef.getDocuments();
-
-  templist = collectionSnapshot.documents; // <--- ERROR
-
-  list = templist.map((DocumentSnapshot docSnapshot){
-    return docSnapshot.data;
-  }).toList();
-
-  return list;
-}  
+  Future<void> pushAddHike(String hikeName, String hikeType) async {
+    mCurrentUser = await _auth.currentUser();
+    if(mCurrentUser.displayName.toString() == null)
+    {
+      Firestore.instance
+      .collection(mCurrentUser.uid)
+      .document("Hikes To Do")
+      .collection("Hike List")
+      .document(hikeName)
+      .setData({'Title': hikeName,'Type': hikeType});
+    }
+    else
+    {
+      Firestore.instance
+      .collection(mCurrentUser.displayName.toString())
+      .document("Hikes To Do")
+      .collection("Hike List")
+      .document(hikeName)
+      .setData({'Title': hikeName,'Type': hikeType});
+    }
 
   }
 }
