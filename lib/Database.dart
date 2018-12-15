935bbcc70fe60127c67cb2b6e2c3f06f2a11d259
Robin <naggirk@plu.edu>
2018-12-15 14:51:06 -0800

    test

--- lib/Database.dart
+++ lib/Database.dart
@@ -2,39 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:firebase_core/firebase_core.dart';
 
-
-class Database{
-
-FirebaseUser mCurrentUser;
-FirebaseAuth _auth = FirebaseAuth.instance;
-
+class Database {
   Future<void> pushAddHike(String hikeName, String hikeType) async {
-    mCurrentUser = await _auth.currentUser();
-    print(mCurrentUser.displayName.toString());
-    print(mCurrentUser.email);
-    if(mCurrentUser.displayName.toString() == null)
-    {
-      Firestore.instance
-      .collection(mCurrentUser.email)
-      .document("Hikes To Do")
-      .collection("Hike List")
-      .document(hikeName)
-      .setData({'Title': hikeName,'Type': hikeType});
-    }
-    else
-    {
-      Firestore.instance
-      .collection(mCurrentUser.displayName.toString())
-      .document("Hikes To Do")
-      .collection("Hike List")
-      .document(hikeName)
-      .setData({'Title': hikeName,'Type': hikeType});
-    }
+    FirebaseUser mCurrentUser = await FirebaseAuth.instance.currentUser();
+    int index = mCurrentUser.email.indexOf('@');
+    String username = mCurrentUser.email.substring(0, index);
+    Firestore.instance
+        .collection(username)
+        .document("Hikes To Do")
+        .collection("Hike List")
+        .document(hikeName)
+        .setData({'Title': hikeName, 'Type': hikeType});
   }
 
-  getProfileImage() async
-  {
-    mCurrentUser = await _auth.currentUser();
+  getProfileImage() async {
+    FirebaseUser mCurrentUser = await FirebaseAuth.instance.currentUser();
     mCurrentUser.photoUrl;
   }
 }
