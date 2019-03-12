6ee8dc29fb75af86c5acd047b1b52870d178437c
capstone <capstone@CSCE16927.cs.plu.edu>
2019-03-12 11:21:46 -0700

    it said pull to merge changes then push if you pull all the saved work will be gone ^not true

--- lib/NewPageToDo.dart
+++ lib/NewPageToDo.dart
@@ -5,6 +5,8 @@ import 'Constants.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'Database.dart';
 
+const Color myColor = Color(0xff243447);
+
 class NewPageToDo extends StatefulWidget {
   NewPageToDoState createState() {
     return NewPageToDoState();
@@ -13,6 +15,113 @@ class NewPageToDo extends StatefulWidget {
 
 class NewPageToDoState extends State<NewPageToDo> {
 
+  openAlertBox(String  title, String description) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(32.0))),
+            contentPadding: EdgeInsets.only(top: 10.0),
+            content: Container(
+              width: 300.0,
+              height: 514.0,
+              //height: 500.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        title,
+                        style: TextStyle(fontSize: 24.0),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    height: 4.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: description,
+                        border: InputBorder.none,
+                      ),
+                      maxLines: 13,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    height: 4.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Edit Length",
+                        border: InputBorder.none,
+                      ),
+                      maxLines: 6,
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    height: 4.0,
+                  ),
+                  InkWell(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                      decoration: BoxDecoration(
+                        color: myColor,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(32.0),
+                            bottomRight: Radius.circular(32.0)),
+                      ),
+                      child: Text(
+                        "Done",
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+ Future<void> _askedToLead() async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return SimpleDialog(
+        contentPadding: const EdgeInsets.all(16.0),
+        title: const Text('Edit Hike'),
+          children: <Widget>[
+          SimpleDialogOption(
+            onPressed: () {},
+            child: const Text('Hike Name: '),
+          ),
+          SimpleDialogOption(
+            onPressed: () {},
+            child: const Text('Hike Description'),
+          ),
+        ],
+      );
+  
+    }
+  );
+}
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -57,15 +166,15 @@ class NewPageToDoState extends State<NewPageToDo> {
                       caption: 'Share',
                       color: Colors.indigo,
                       icon: Icons.share,
-                      onTap: () {} ,
+                      onTap: () {}    
                     ),
                   ],
                   secondaryActions: <Widget>[
                     new IconSlideAction(
-                      caption: 'More',
+                      caption: 'Edit',
                       color: Colors.black45,
                       icon: Icons.more_horiz,
-                      onTap: () {},
+                      onTap: () {openAlertBox(document['Title'],document['Type'] );},
                     ),
                     new IconSlideAction(
                       caption: 'Delete',
