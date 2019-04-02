ad4b8f5f5c6e90e16b0b095250a7f2688d852e5e
capstone <capstone@CSCE16927.cs.plu.edu>
2019-04-02 12:37:44 -0700

    Robins changes to his tree so that i can switch

--- lib/NewPageMap.dart
+++ lib/NewPageMap.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 import 'Home.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Constants.dart';
 
 
 var myKey = 'AIzaSyB_2tJ1sapz6JjEYRCA2fVYQc6TM_LbMAI';
- var currentLocation;
 
 class NewPageMap extends StatefulWidget {
   NewPageMapState createState() {
@@ -17,35 +17,51 @@ class NewPageMap extends StatefulWidget {
 class NewPageMapState extends State<NewPageMap> {
   MapView mapView = new MapView();
   
-  void initState() {
-    super.initState();
-    Geolocator().getCurrentPosition().then((currloc) {
-      setState(() {
-        currentLocation = currloc;
-      });
-    });
-  }
 
-  List<Marker> markers = <Marker>[
-    new Marker("1", "Name of Hike", 20.2672, -97.7431, color: Colors.blue, draggable: true),
+  List<Marker> markers = [
+    /*new Marker("1", "Name of Hike", 20.2672, -97.7431, color: Colors.blue, draggable: true),
     new Marker("2", "Name of Hike", 30.2672, -97.7431, color: Colors.blue, draggable: true),
     new Marker("3", "Name of Hike", 40.2672, -97.7431, color: Colors.blue, draggable: true),
-    new Marker("4", "Name of Hike", 50.2672, -97.7431, color: Colors.blue, draggable: true)
+    new Marker("4", "Name of Hike", 50.2672, -97.7431, color: Colors.blue, draggable: true)*/
   ]; 
 
+  Widget loadMarker() {
+    print("IN THE LOAD MARKER METHOD");
+    return StreamBuilder(
+      stream: Firestore.instance.collection(globalUserName).document('Done Hikes').collection('Hike List').snapshots(),
+      builder: (context, snapshot) {
+        print("IN THE BUILDER METHOD");
+        if(snapshot.hasError) {
+          print(snapshot.error);
+        }
+        if(!snapshot.hasData) return Text('Loading markers');
+        for(int i=0; i < snapshot.data.document.length; i++) {
+          int num = i + 1;
+          print("$num");
+          print(snapshot.data.document['Title']);
+          print(snapshot.data.document['Latitude']);
+          print(snapshot.data.document['Longitude']);
+          markers.add(new Marker("$num",snapshot.data.document['Title'], 
+                                      snapshot.data.document['Latitude'], 
+                                      snapshot.data.document['Longitude']));
+        }
+      }, 
+    );
+  }
+
   displayMap() {
+    loadMarker();
     mapView.show(new MapOptions(
       mapViewType: MapViewType.normal,
-      //initialCameraPosition: new CameraPosition(new Location(47.6062, -122.3321), 8.0),
-      initialCameraPosition: new CameraPosition(currentLocation, 8.0),
+      initialCameraPosition: new CameraPosition(new Location(47.6062, -122.3321), 8.0),
       showUserLocation: true,
       title: 'Hike Locator',
-      
     ));
     //mapView.setMarkers(markers);
     //mapView.zoomToFit(padding: 100);
     mapView.onMapTapped.listen((tapped) {
       //call the database class to let all the lgn and lat to put in the markers
+      loadMarker();
       mapView.setMarkers(markers);
       mapView.zoomToFit(padding: 100);
     });
