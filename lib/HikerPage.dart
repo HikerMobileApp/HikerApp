155d2a460a54b8b26dc6c638eb8c67dcb6751287
Isaiah Scheel <isaiahscheel@gmail.com>
2019-04-11 14:53:59 -0700

    Sprint 7

--- lib/HikerPage.dart
+++ lib/HikerPage.dart
@@ -4,14 +4,15 @@ import 'Database.dart';
 import 'StatCard.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-
 class HikerPage extends StatefulWidget {
   final String name;
   final String profPic;
   final String miles;
-   HikerPage({Key key, @required this.name, this.profPic, this.miles});
-  _HikerPage createState() => new _HikerPage(username: name, profilePic: profPic, milesHiked: miles);
+  HikerPage({Key key, @required this.name, this.profPic, this.miles});
+  _HikerPage createState() =>
+      new _HikerPage(username: name, profilePic: profPic, milesHiked: miles);
 }
+
 int doneHikes;
 int todoHikes;
 double milesHiked;
@@ -21,176 +22,206 @@ List<DocumentSnapshot> doneHikesReturn;
 Database temp = new Database();
 String icon;
 
-Container hikecardToDo(String hikeName, String hikeType, String miles, var context){
-  if(hikeType == "Multi-Night"){
-      icon = "weatherNight";
+Container hikecardToDo(
+    String hikeName, String hikeType, String miles, var context) {
+  if (hikeType == "Multi-Night") {
+    icon = "weatherNight";
+  } else if (hikeType == "Day Hike") {
+    icon = "walk";
+  } else if (hikeType == "Backpacking") {
+    icon = "tent";
+  } else {
+    icon = "pineTree";
   }
-  else if(hikeType == "Day Hike"){
-      icon = "walk";
-  }
-  else if(hikeType == "Backpacking"){
-      icon = "tent";
-  }
-  else{
-      icon = "pineTree";
-  }
-  return 
-      Container(
-        //color: Colors.white,
-        width: context.size.width/2,
-        decoration: BoxDecoration(border: Border.all(
-            color: lightDark,
-            width: 8.0,
-          ),
-          color: Colors.white
-          ),
-        //height: 20,
-        //width: 20,
-        child: new ListTile(
-            leading: Icon(MdiIcons.fromString(icon)),
-            title:  Text(hikeName),
-            subtitle:  Text(miles + ' mile ' + hikeType),
-            ),
-      );
+  return Container(
+    width: context.size.width / 2,
+    //padding: EdgeInsets.all(8.0),
+    decoration: BoxDecoration(
+        color: lightDark),
+    child: 
+    new Card(
+      child: 
+      new ListTile(
+      leading: Icon(MdiIcons.fromString(icon)),
+      title: Text(hikeName),
+      subtitle: Text(miles + ' mile \n' + hikeType),
+      isThreeLine: true,
+    ),
+    ),
+  );
 }
 
 class _HikerPage extends State<HikerPage> {
-final String username;
-final String profilePic;
-final String milesHiked;
- _HikerPage({Key key, @required this.username, this.profilePic, this.milesHiked});
+  final String username;
+  final String profilePic;
+  final String milesHiked;
+  _HikerPage(
+      {Key key, @required this.username, this.profilePic, this.milesHiked});
 
   @override
   void initState() {
     super.initState();
   }
 
-
-@override
+  @override
   Widget build(BuildContext context) {
-  var mediaQuery = MediaQuery.of(context);
+    var mediaQuery = MediaQuery.of(context);
     return new Scaffold(
         appBar: new AppBar(
-              backgroundColor: lightDark,
-              title: new Text(username),
-              actions: <Widget>[
-              ],
+          backgroundColor: lightDark,
+          title: new Text(username),
+          actions: <Widget>[],
         ),
-        body: new Stack(
-      children: <Widget>[
-        ClipPath(
-          child: Container(color: Colors.black.withOpacity(0.8)),
-          clipper: getClipper(),
-        ),
-        //Center(
-        Positioned(
-            width: MediaQuery.of(context).size.width,
-            top: MediaQuery.of(context).size.height / 10,
-            child: Column(
-              children: <Widget>[
-                Container(
-                    width: 150.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        image: DecorationImage(
-                            image: new NetworkImage(profilePic),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
-                        boxShadow: [
-                          BoxShadow(blurRadius: 7.0, color: Colors.black)
-                        ])),
-                SizedBox(height: 1.0),
-                Text(
-                  username,
-                  style: TextStyle(
-                    color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat'),
-                ),
-                SizedBox(height: 15.0),
-                Text(
-                  'Stats: ',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17.0,
-                      fontFamily: 'Montserrat'),
-                ),
-                SizedBox(height: 25.0),
-                
-                  
-              Column(
-                  children: <Widget>[
-                    Container(
-                  width: MediaQuery.of(context).size.width/2,
-                  child:
-                      statCardMaker("Miles Hiked", milesHiked.toString()),
-                    ),
-                     Container(
-                        
-                        child: StreamBuilder<QuerySnapshot>(
-                        stream: Firestore.instance
-                          .collection(username)
-                          .document("Hikes To Do")
-                          .collection("Hike List")
-                          .snapshots(),
+        body: new Stack(children: <Widget>[
+          ClipPath(
+            child: Container(color: Colors.black.withOpacity(0.8)),
+            clipper: getClipper(),
+          ),
+          Positioned(
+              width: MediaQuery.of(context).size.width,
+              top: MediaQuery.of(context).size.height / 10,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      width: 150.0,
+                      height: 150.0,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          image: DecorationImage(
+                              image: new NetworkImage(profilePic),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                          boxShadow: [
+                            BoxShadow(blurRadius: 7.0, color: Colors.black)
+                          ])),
+                  SizedBox(height: 1.0),
+                  Text(
+                    username,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat'),
+                  ),
+                  SizedBox(height: 15.0),
+                  Text(
+                    'Stats: ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17.0,
+                        fontFamily: 'Montserrat'),
+                  ),
+                  SizedBox(height: 25.0),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child:
+                            statCardMaker("Miles Hiked", milesHiked.toString()),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                                child: new Text("Hikes to-do", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),),
+                                height: 25.0),
+                      Container(
+                          child: StreamBuilder<QuerySnapshot>(
+                              stream: Firestore.instance
+                                  .collection(username)
+                                  .document("Hikes To Do")
+                                  .collection("Hike List")
+                                  .snapshots(),
+                              builder: (context, snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Container(
+                                      child: Center(child: Text("No Hikes")));
+                                }
+                                return Container(
+                                    height:
+                                        MediaQuery.of(context).size.width / 4,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: ListView.builder(
+                                        //padding: EdgeInsets.all(8.0),
+                                        reverse: false,
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount:
+                                            snapshot.data.documents.length,
+                                        itemBuilder: (_, int index) {
+                                          String hikeName = snapshot
+                                              .data.documents[index]["Title"];
+                                          String hikeType = snapshot
+                                              .data.documents[index]["Type"];
+                                          String miles = snapshot
+                                              .data.documents[index]["Miles"];
+                                          return new GestureDetector(
+                                            onTap: () {},
+                                            child: hikecardToDo(hikeName,
+                                                hikeType, miles, mediaQuery),
+                                          );
+                                        }));
+                              })),
+                              SizedBox(height: 10),
+                              SizedBox(
+                                child: new Text("Done Hikes", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),),
+                                height: 25.0),
 
-                        builder: (context, snapshot){
-                      if (!snapshot.hasData){
-                        return Container(
-                          child: Center(
-                            child: Text("No Hikes")
-                          )
-                        );
-                      }
-                        return Container(
-                          height: MediaQuery.of(context).size.width/5,
-                          width: MediaQuery.of(context).size.width,
-                          child: ListView.builder(
-                      padding: EdgeInsets.all(1.0),
-                      reverse: false,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (_, int index) {
-                        String hikeName = snapshot.data.documents[index]["Title"];
-                        String hikeType = snapshot.data.documents[index]["Type"];
-                        String miles = snapshot.data.documents[index]["Miles"];
-                        return 
-                          new GestureDetector(
-                            onTap: () {
-                            },
-                            child:
-                              hikecardToDo(hikeName, hikeType, miles, mediaQuery),
-                              );
-                          
-                      }
-                    ) 
-                        );    
-                  }
-
-            
-                
-                )
-                      ) 
-              ],
-            )
-      ],
-    ))
-      ]
-        )
-    );
+                              Container(
+                          child: StreamBuilder<QuerySnapshot>(
+                              stream: Firestore.instance
+                                  .collection(username)
+                                  .document("Done Hikes")
+                                  .collection("Hike List")
+                                  .snapshots(),
+                              builder: (context, snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Container(
+                                      child: Center(child: Text("No Hikes")));
+                                }
+                                return Container(
+                                    height:
+                                        MediaQuery.of(context).size.width / 4,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: ListView.builder(
+                                        //padding: EdgeInsets.all(8.0),
+                                        reverse: false,
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount:
+                                            snapshot.data.documents.length,
+                                        itemBuilder: (_, int index) {
+                                          String hikeName = snapshot
+                                              .data.documents[index]["Title"];
+                                          String hikeType = snapshot
+                                              .data.documents[index]["Type"];
+                                          String miles = snapshot
+                                              .data.documents[index]["Miles"];
+                                          return new GestureDetector(
+                                            onTap: () {},
+                                            child: hikecardToDo(hikeName,
+                                                hikeType, miles, mediaQuery),
+                                          );
+                                        }));
+                              })),
+                    ],
+                  )
+                ],
+              ))
+        ]));
   }
 }
-
-
 
 class getClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = new Path();
 
-    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(0.0, size.height / 2);
     path.lineTo(size.width + 125, 0.0);
     path.close();
     return path;
@@ -198,8 +229,6 @@ class getClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
     return true;
   }
 }
-  
--- lib/Hikers.dart
+++ lib/Hikers.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'main.dart';
 import 'Database.dart';
 import 'HikerPage.dart';
 
@@ -9,87 +8,74 @@ class Hikers extends StatefulWidget {
   _Hikers createState() => new _Hikers();
 }
 
-  Database temp = new Database();
-Card profileCard(String name, var miles, String profPic){
-  if(miles == null){
-     miles = 0;
+Database temp = new Database();
+Card profileCard(String name, var miles, String profPic) {
+  if (miles == null) {
+    miles = 0;
   }
-  if(profPic == ""){
-    profPic = "https://amp.businessinsider.com/images/5899ffcf6e09a897008b5c04-750-750.jpg";
+  if (profPic == "") {
+    profPic =
+        "https://amp.businessinsider.com/images/5899ffcf6e09a897008b5c04-750-750.jpg";
   }
   return new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-             ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(profPic)),
-            title:  Text(name),
-            subtitle: new Text("Miles Hiked: " + miles.toString()),
-            //subtitle:  Text(miles + ' mile ' + hikeType),
-            ),
-          ]
-        )
-      );
+      child: new Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+    ListTile(
+      leading: CircleAvatar(backgroundImage: NetworkImage(profPic)),
+      title: Text(name),
+      subtitle: new Text("Miles Hiked: " + miles.toString()),
+    ),
+  ]));
 }
 
-class _Hikers extends State<Hikers>{
+class _Hikers extends State<Hikers> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
-            @override
-            void initState() {
-              super.initState();
-            }
-
-Widget build(BuildContext context) {
-  Color lightDark = Color(0xff243447);
-          return new Scaffold(
-            appBar: new AppBar(
-              backgroundColor: lightDark,
-              title: new Text("Hikers"),
-              actions: <Widget>[
-              ],
-            ),
-              body: new Column(
-              children: <Widget>[
-                new Flexible(
-                  child: StreamBuilder(
-                    stream: Firestore.instance.collection("USERS").snapshots(),
-                    builder: (context, snapshot){
-                      if (!snapshot.hasData){
-                        return Container(
-                          child: Center(
-                            child: Text("No data")
-                          )
-                        );
-                      }
+  Widget build(BuildContext context) {
+    Color lightDark = Color(0xff243447);
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: lightDark,
+          title: new Text("Hikers"),
+          actions: <Widget>[],
+        ),
+        body: new Column(children: <Widget>[
+          new Flexible(
+              child: StreamBuilder(
+                  stream: Firestore.instance.collection("USERS").snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Container(child: Center(child: Text("No data")));
+                    }
 
                     return ListView.builder(
-                      padding: EdgeInsets.all(8.0),
-                      reverse: false,
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (_, int index) {
-                        String user = snapshot.data.documents[index]["Name"];
-                        var miles = snapshot.data.documents[index]["MilesHiked"];
-                        String profPic = snapshot.data.documents[index]["profilePic"];
-                        return 
-                          new GestureDetector(
+                        padding: EdgeInsets.all(8.0),
+                        reverse: false,
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (_, int index) {
+                          String user = snapshot.data.documents[index]["Name"];
+                          var miles =
+                              snapshot.data.documents[index]["MilesHiked"];
+                          String profPic =
+                              snapshot.data.documents[index]["profilePic"];
+                          return new GestureDetector(
                             onTap: () {
                               Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => HikerPage(name: user, profPic: profPic, miles: miles.toString(),)),
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HikerPage(
+                                          name: user,
+                                          profPic: profPic,
+                                          miles: miles.toString(),
+                                        )),
                               );
                             },
-                            child:
-                           profileCard(user, miles, profPic),
+                            child: profileCard(user, miles, profPic),
                           );
-                      }
-                    );        
-                  }
-                )
-
-                  ),
-                ]
-                        )
-                        );
-              }
-              }
+                        });
+                  })),
+        ]));
+  }
+}
