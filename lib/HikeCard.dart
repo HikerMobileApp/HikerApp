8b2956a3909f459ef7b7a6ecc827f5127ace2d4d
capstone <capstone@CSCE16927.cs.plu.edu>
2019-04-14 12:19:36 -0700

    Images init

--- lib/HikeCard.dart
+++ lib/HikeCard.dart
@@ -1,139 +1,164 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
-
-//class HikeCard(String hikeName, string hikeType, int elevation, String location){
-//
-//}
+import 'package:expandable/expandable.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:image_picker/image_picker.dart';
 
 Icon leadingIcon;
 String icon;
+File _image;
 
-Card toDoHikeCardMaker(String hikeName, String hikeType, String miles){
-  if(hikeType == "Multi-Night"){
-      icon = "weatherNight";
-  }
-  else if(hikeType == "Day Hike"){
-      icon = "walk";
-  }
-  else if(hikeType == "Backpacking"){
-      icon = "tent";
-  }
-  else{
-      icon = "pineTree";
+Card toDoHikeCardMaker(String hikeName, String hikeType, String miles) {
+  if (hikeType == "Multi-Night") {
+    icon = "weatherNight";
+  } else if (hikeType == "Day Hike") {
+    icon = "walk";
+  } else if (hikeType == "Backpacking") {
+    icon = "tent";
+  } else {
+    icon = "pineTree";
   }
   return new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-             ListTile(
-            leading: Icon(MdiIcons.fromString(icon)),
-            title:  Text(hikeName),
-            subtitle:  Text(miles + ' mile ' + hikeType),
-            ),
-            /*new ButtonTheme.bar(
-              child: new ButtonBar(
-                children: <Widget>[
-                  new FlatButton(
-                    child: const Text('DONE', style: TextStyle(color: Colors.blueGrey)),
-                    onPressed: () {
-                      Database temp = new Database();
-                      temp.pushAddDoneHike(hikeName, hikeType);
-                      temp.deleteHikeFromToDoPage(hikeName);
-                      }
-                  ),
-                  new FlatButton(
-                    child: const Text('DELETE', style: TextStyle(color: Colors.blueGrey)),
-                    onPressed: () {
-                      Database temp = new Database();
-                      temp.deleteHikeFromToDoPage(hikeName);
-                    }
-                  ),
-                ],
-              )
-            )*/
-          ]
-        )
-      );
+      child: new Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+    ListTile(
+      leading: Icon(MdiIcons.fromString(icon)),
+      title: Text(hikeName),
+      subtitle: Text(miles + ' mile ' + hikeType),
+    ),
+  ]));
 }
 
-Card doneHikeCardMaker(String hikeName, String hikeType, String miles, String des, String long, String lat, String date){
-    if(hikeType == "Multi-Night"){
-      icon = "weatherNight";
+Card doneHikeCardMaker(String hikeName, String hikeType, String miles,
+    String des, String long, String lat, String date) {
+  if (hikeType == "Multi-Night") {
+    icon = "weatherNight";
+  } else if (hikeType == "Day Hike") {
+    icon = "walk";
+  } else if (hikeType == "Backpacking") {
+    icon = "tent";
+  } else {
+    icon = "pineTree";
   }
-  else if(hikeType == "Day Hike"){
-      icon = "walk";
-  }
-  else if(hikeType == "Backpacking"){
-      icon = "tent";
-  }
-  else{
-      icon = "pineTree";
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    _image = image;
   }
   return new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-             ListTile(
-            leading:  Icon(MdiIcons.fromString(icon)),
-            title:  Text(hikeName),
-            subtitle:  Text(miles + ' mile(s)\t Type: ' + hikeType +'\tTrip Description: ' + des + '\tLongitude: '+ long + ', Latitude: '+ lat + '\t Date: ' +  date),
+    child: ExpandableNotifier(
+      controller: ExpandableController(false),
+      child: new Column(
+        mainAxisSize: MainAxisSize.min, 
+        children: <Widget>[
+          Expandable(
+            collapsed: 
+            ListTile(
+              leading: Icon(MdiIcons.fromString(icon)),
+              title: Text(hikeName),
+              subtitle: Text(miles +
+                  ' mile(s)\t Type: ' +
+                  hikeType +
+                  '\nTrip Description: ' +
+                  des +
+                  '\nLongitude: ' +
+                  long +
+                  ', Latitude: ' +
+                  lat +
+                  '\nDate: ' +
+                  date),
             ),
-            /*new ButtonTheme.bar(
-              child: new ButtonBar(
-                children: <Widget>[
-                  new FlatButton(
-                    child: const Text('EDIT', style: TextStyle(color: Colors.blueGrey)),
-                    onPressed: () {
-                      Database temp = new Database();
-                      //temp.pushAddDoneHike(hikeName, hikeType);
-                      temp.deleteHikeFromDonePage(hikeName);
-                      }
+            expanded: 
+            ListTile(
+                leading: Icon(MdiIcons.fromString(icon)),
+                title: Text(hikeName),
+                subtitle:
+                new SizedBox(
+                  height: 400.0,
+                  width: 300.0,
+                  child: _image == null
+                  ? new Carousel(
+                    images: [
+                      new NetworkImage('https://golutes.com/images/2018/12/11/TF_Scheel_web.jpg?width=300'),
+                      new NetworkImage('https://a2-images.myspacecdn.com/images03/33/b98cedc7bf1d42339c9d37ec03ab1fbc/300x300.jpg'),
+                      
+                    
+                      
+                    ],
+                  )
+                  : new Carousel(
+                    images: [
+                      new NetworkImage('https://golutes.com/images/2018/12/11/TF_Scheel_web.jpg?width=300'),
+                      new NetworkImage('https://a2-images.myspacecdn.com/images03/33/b98cedc7bf1d42339c9d37ec03ab1fbc/300x300.jpg'),
+                      new Image.file(_image),
+                    
+                      
+                    ],
                   ),
-                  new FlatButton(
-                    child: const Text('DELETE', style: TextStyle(color: Colors.blueGrey)),
-                    onPressed: () {
-                      Database temp = new Database();
-                      temp.deleteHikeFromDonePage(hikeName);
-                    }
                   ),
-                ],
-              )
-            )*/
-          ]
+                trailing:FloatingActionButton(
+                  onPressed: getImage,
+                  tooltip: 'Pick Image',
+                  child: Icon(Icons.add_a_photo),
+                ),
+                 
+              
+                
+
+              
+            ), 
+          ),
+    
+    Divider(
+      height: 0.0,
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Builder(
+          builder: (context){
+            var exp = ExpandableController.of(context);
+            return MaterialButton(
+              child: Text(exp.expanded ?"Info": "Photos",
+              style: Theme.of(context).textTheme.button.copyWith(
+                color:Colors.lightGreen
+              ),
+              ),
+              onPressed: (){
+                exp.toggle();
+              },
+            );
+          }
         )
-      );
+      ],)
+
+
+  ])));
 }
 
-class HikeCard extends StatelessWidget{
+class HikeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Card(
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-            leading: const Icon(Icons.directions_walk),
-            title: const Text('Jade Lake'),
-            subtitle: const Text('20 mile Backpacking'),
-            ),
-            new ButtonTheme.bar(
-              child: new ButtonBar(
-                children: <Widget>[
-                  new FlatButton(
-                    child: const Text('DONE', style: TextStyle(color: Colors.blueGrey),),
-                    onPressed: () {}
-                  ),
-                  new FlatButton(
-                    child: const Text('DELETE'),
-                    onPressed: () {}
-                  ),
-                ],
-              )
-            )
-          ]
-        )
-      );
- 
+        child: new Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      const ListTile(
+        leading: const Icon(Icons.directions_walk),
+        title: const Text('Jade Lake'),
+        subtitle: const Text('20 mile Backpacking'),
+      ),
+      new ButtonTheme.bar(
+          child: new ButtonBar(
+        children: <Widget>[
+          new FlatButton(
+              child: const Text(
+                'DONE',
+                style: TextStyle(color: Colors.blueGrey),
+              ),
+              onPressed: () {}),
+          new FlatButton(child: const Text('DELETE'), onPressed: () {}),
+        ],
+      ))
+    ]));
   }
 }
